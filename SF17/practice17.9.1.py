aa785dc798a8db8a9b2eb00662fe49d21cf0910e
iratwinklle <iratwinkle@gmail.com>
2022-04-28 16:55:25 +0300

    practice SF 17

--- SF17/practice17.9.1.py
+++ SF17/practice17.9.1.py
@@ -2,6 +2,10 @@ def binary_search(list, item):
   low = 0
   high = len(list) - 1
   i = 0
+  if list[high] < item:
+      return False
+  if list[low] >= item:
+      return False
   while low <= high:
     mid = (low + high) // 2
     if list[mid] < item and list[mid +1] >= item:
@@ -12,18 +16,33 @@ def binary_search(list, item):
       low = mid + 1
     i=i+1
     return None
+def bubble_sort(list):
+    for i in range(len(list)):
+        for j in range(len(list) - i - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+def seq_check_clear(string):
+    seq_list = string.split(' ')
+    try:
+        int_list = list(map(int,seq_list))
+        return int_list
+    except:
+        return False
+
 seq = input('Введите последовательность чисел через пробел:')
-number = int(input('Введите любое число:'))
-seq_clear = list(map(int, seq.split()))
-for i in range(len(seq_clear)):
-    for j in range(len(seq_clear) - i - 1):
-        if seq_clear[j] > seq_clear[j + 1]:
-            seq_clear[j], seq_clear[j + 1] = seq_clear[j + 1], seq_clear[j]
-print(seq_clear)
-print(binary_search(seq_clear, number))
-
-
-
-
-
+number = input('Введите любое число:')
+seq_clear = seq_check_clear(seq)
+number_check = seq_check_clear(number)
+if seq_clear and number_check:
+    sorted_seq = bubble_sort(seq_clear)
+    print('Список введенных чисел по возрастанию: {}'.format(sorted_seq))
+    index = binary_search(sorted_seq, number_check)
+    if index:
+        print('Номер позиции элемента, который меньше введенного пользователем числа, '
+              'а следующий за ним больше или равен этому числу: {}'.format(index))
+    else:
+        print('Введенное число находится за пределами последовательности')
+else:
+    print("Введенные данные не соответствуют требуемым условиям")
 
